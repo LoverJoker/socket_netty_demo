3738afdb5845c193f3de6cd189774e88d53dea23
JokerLiang <we@jokerliang.cn>
2020-06-18 17:30:53 +0800

    update

--- src/main/java/com/jokerliang/socket_netty_demo/TestController.java
+++ src/main/java/com/jokerliang/socket_netty_demo/TestController.java
@@ -79,14 +79,14 @@ public class TestController {
     @GetMapping("/volume")
     public String volume(String deviceCode, int num) {
         byte[] bytes = GarshponMachine.Params.setVolume(num);
-        ServerMessageHandler.sendMessage("DD", bytes);
+        ServerMessageHandler.sendMessage(deviceCode, bytes);
         return  "发送的数据：" + ByteUtils.byteArrayToHexString(bytes);
     }
 
     @GetMapping("/getVolume")
     public String getVolume(String deviceCode) {
         byte[] bytes = GarshponMachine.Params.queryVolume();
-        ServerMessageHandler.sendMessage("DD", bytes);
+        ServerMessageHandler.sendMessage(deviceCode, bytes);
         return  "发送的数据：" + ByteUtils.byteArrayToHexString(bytes);
     }
 
